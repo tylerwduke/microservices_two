6ff05bbc46e92338bc58fa94f0e4ec611887db1d
tylerdukedev <tylerdukedev@gmail.com>
2023-12-24 15:10:37 -0500

    fixed useeffect infinite request

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -7,14 +7,13 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts/');
-
+        const res = await axios.get('http://localhost:4002/posts/');
         setPosts(res.data);
     }
 
     useEffect(() => {
         fetchPosts();
-    }, [posts])
+    }, [])
 
     const renderedPosts = Object.values(posts).map(post => {
         return (
@@ -24,7 +23,7 @@ const PostList = () => {
                     <CommentCreate postId={post.id} />
                 </div>
                 <div style={{ display: 'flex', justifyContent:'center' }}>
-                    <CommentList postId={post.id} />
+                    <CommentList comments={post.comments} />
                 </div>
              </div>
         )
